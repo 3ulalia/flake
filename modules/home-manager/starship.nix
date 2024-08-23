9f4f47dbbd323461c5efaadb2290880612ededcf
3ulalia <179992797+3ulalia@users.noreply.github.com>
2024-08-23 09:23:12 -0400

    add starship (unconfigured)

---
+++ modules/home-manager/starship.nix
@@ -0,0 +1,22 @@
+{
+  config,
+  lib,
+  osConfig,
+  ...
+} :
+  let 
+    inherit (osConfig.eula.lib.options) mkOpt;
+    inherit (lib) mkIf types;
+  in { 
+    options.eula.modules.home-manager.starship = {
+      enable = mkOpt types.bool false;
+    };
+
+    config = mkIf config.eula.modules.home-manager.starship.enable {
+      programs.starship = {
+	enable = true;
+	enableZshIntegration = true;
+	# TODO add configuration options to the home-manager module
+      };
+    };
+}
