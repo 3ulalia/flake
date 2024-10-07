69f3a270378cf895cc733cfcf87a33950c3b16c7
3ulalia <179992797+3ulalia@users.noreply.github.com>
2024-10-07 12:47:36 -0400

    niri: _really_ clean up nixos module

--- modules/nixos/niri.nix
+++ modules/nixos/niri.nix
@@ -3,37 +3,17 @@
   inputs,
   lib,
   pkgs,
+  niri,
   ...
 } : 
   let 
-    inherit (builtins) length;
-    inherit (lib) mkOption mkIf mkAliasDefinitions trace types;
-    inherit (config.eula.lib.helpers) list-to-attrs;
-    inherit (config.eula.lib.options) mkOpt;
+    inherit (config.eula.lib.modules) any-user;
   in {
 
-    imports = [(trace "importing niri nixos module" inputs.niri.nixosModules.niri)];
+    imports = [ inputs.niri.nixosModules.niri ];
 
-    options.eula.modules.nixos.niri.enableFor = mkOpt (types.listOf types.str) [];
+    nixpkgs.overlays = [inputs.niri.overlays.niri];
     
-    config = mkIf ((length config.eula.modules.nixos.niri.enableFor) != 0) rec {
-      programs.niri.enable = (trace "niri is enabled systemwide!" true);
-      programs.niri.package = pkgs.niri-unstable; 
-      security.pam.services.swaylock = {}; # TODO
-      nixpkgs.overlays = [inputs.niri.overlays.niri];
-
-      # TODO move
-      fonts.packages = [
-	pkgs.meslo-lg
-	(pkgs.nerdfonts.override { fonts = ["Meslo"]; })
-      ];
-
-      home-manager.users = 
-	list-to-attrs (
-	  map 
-	    (name: {${name} = {config.eula.modules.home-manager.niri.enable = true;};})
-	    (trace "mapping over users to enable niri accordingly!" config.eula.modules.nixos.niri.enableFor)
-	);
-    };
+    programs.niri.enable = (any-user (user: user.eula.modules.home-manager.niri.enable) a);
 }
 	      
