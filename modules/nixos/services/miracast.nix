f274f2ffb7cd6b03a8961c8b2b3cc9f08ba3a831
3ulalia <179992797+3ulalia@users.noreply.github.com>
2024-10-12 10:50:49 -0400

    add miracast service (not currently working on t2

---
+++ modules/nixos/services/miracast.nix
@@ -0,0 +1,23 @@
+{
+  config,
+  inputs,
+  lib,
+  pkgs,
+  ...
+} : 
+  let 
+    inherit (config.eula.lib.options) mkOpt;
+    inherit (lib) mkIf types;
+  in {    
+    options.eula.modules.services.miracast = {
+      enable = mkOpt types.bool false;
+    };
+    
+    config = mkIf config.eula.modules.services.miracast.enable {
+      environment.systemPackages = [ pkgs.gnome-network-displays ];
+      networking.firewall = mkIf config.networking.firewall.enable {
+	allowedTCPPorts = [7236 7250];
+	allowedUDPPorts = [7236 5353];
+      };
+    };
+}
