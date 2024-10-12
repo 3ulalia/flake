5bc2cf1d01fd63b9e0fa47b6cb5c223c8fe8fff8
3ulalia <179992797+3ulalia@users.noreply.github.com>
2024-10-12 10:51:02 -0400

    add basic SSH service

---
+++ modules/nixos/services/ssh.nix
@@ -0,0 +1,31 @@
+{
+  config,
+  inputs,
+  lib,
+  pkgs,
+  ...
+} : 
+  let 
+    inherit (config.eula.lib.options) mkOpt;
+    inherit (lib) mkIf types;
+  in {
+    options.eula.modules.services.ssh = {
+      enable = mkOpt types.bool false;
+      port = mkOpt types.port 22;
+    };
+
+    config = mkIf config.eula.modules.services.ssh.enable {
+      services.openssh = {
+	enable = true;
+	ports = [ config.eula.modules.services.ssh.port ];
+	settings = {
+	  PasswordAuthentication = true; # TODO
+	  AllowUsers = null;
+	};
+      };
+
+      networking.firewall.allowedTCPPorts = mkIf config.networking.firewall.enable [
+	config.eula.modules.services.ssh.port
+      ];
+   };
+}
