9ebbdc10ea714efc4e00e5d6bf704857063ed37c
3ulalia <179992797+3ulalia@users.noreply.github.com>
2025-01-08 13:16:31 -0500

    better avizo styling; stop installing nerdfonts with waybar

--- users/eulalia/home/avizo.nix
+++ users/eulalia/home/avizo.nix
@@ -4,11 +4,14 @@
   lib,
   pkgs,
   ...
-} : {
-
+}: {
   config.services.avizo.settings = {
     default = {
+      time = 2;
       padding = 10;
+      width = 256;
+      height = 128;
+      background = "rgba(160,160,160,0.5)";
     };
-  }
+  };
 }
