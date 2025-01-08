c5d5bdbfe9b9baa2a038a4d5a2c7163338961096
3ulalia <179992797+3ulalia@users.noreply.github.com>
2025-01-08 13:16:20 -0500

    waybar: stop using nerdfonts, start using tray

--- users/eulalia/home/waybar.nix
+++ users/eulalia/home/waybar.nix
@@ -4,30 +4,29 @@
   lib,
   pkgs,
   ...
-} : {
+}: {
+  programs.waybar = {
+    enable = true;
+    systemd.enable = true;
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "top";
+        height = 30;
 
-    programs.waybar = {
-      enable = true;
-      systemd.enable = true;
-      settings = { mainBar = {
-	layer = "top";
-	position = "top";	
-	height = 30;
+        modules-center = ["clock" "battery"];
 
-	modules-center = ["clock" "battery" "network" "wlr/taskbar" "niri/workspaces" "tray"];
+        modules-left = ["niri/workspaces" "wlr/taskbar"];
 
-	"battery" = {
-	  "tooltip-format" = "{time}";
-	};
-	"clock" = {
-	  "tooltip-format" = "{:%c}";  
-	};
-	"network" = {
-	  "format-wifi" = "{icon}";
-	  "format-icons" = ["󰤟" "󰤢" "󰤥" "󰤨"];
-	  "tooltip-format-wifi" = "{essid} ({signalStrength}%)";
-	};
-     }; };
+        modules-right = ["tray"];
 
+        "battery" = {
+          "tooltip-format" = "{time}";
+        };
+        "clock" = {
+          "tooltip-format" = "{:%c}";
+        };
+      };
     };
+  };
 }
