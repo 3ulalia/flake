28856e286ba3bd4b35a48a679ae30084da9223b4
3ulalia <179992797+3ulalia@users.noreply.github.com>
2025-01-08 13:16:03 -0500

    silly fastfetch config (TODO: fix uptime)

--- users/eulalia/home/fastfetch.nix
+++ users/eulalia/home/fastfetch.nix
@@ -2,5 +2,41 @@
 
   programs.fastfetch = {
     enable = true;
+    settings = {
+      modules = [
+        "title"
+        "separator"
+        "os"
+        "host"
+        {
+          type = "kernel";
+          format = "{release}";
+        }
+        "uptime"
+        {
+          type = "command";
+          key = "Uptime";
+          text = "echo $(($(($(date +%s) - 1707777777)) / 60 / 60 / 24)),$((($(($(date +%s) - 1707777777)) / 60 / 60) % 24)),$((($(($(date +%s) - 1707777777)) / 60) % 60))";
+          format = "{#cyan}{1~0,3} days, {#magenta}{1~4,6} hours, {#white}{1~7} mins"; 
+        }
+        "packages"
+        "shell"
+        "de"
+        "terminal"
+        "cpu"
+        {
+            type = "gpu";
+            key = "GPU";
+        }
+        {
+            type = "memory";
+            format = "{} / {}";
+        }
+        "btrfs"
+        "locale"
+        "break"
+        "colors"
+      ];
+    };
   };
 }
