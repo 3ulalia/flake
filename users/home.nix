cb988f0239d315ccd2db44b8ba1a53170f9e29a0
3ulalia <179992797+3ulalia@users.noreply.github.com>
2025-01-08 13:47:39 -0500

    add more zsh features

--- users/home.nix
+++ users/home.nix
@@ -16,10 +16,11 @@ in {
   # into using home-manager, we _should_ always have git in our environment.
   # should.
   programs.git.enable = true;
-  programs.home-manager.enable = (trace "evaluating users/home.nix!" true);
+  programs.home-manager.enable = trace "evaluating users/home.nix!" true;
 
-  programs.zsh.enable = mkIf zsh-enable true;
-  programs.zsh.initExtra = mkIf zsh-enable "any-nix-shell zsh --info-right | source /dev/stdin";
-  home = mkIf zsh-enable {packages = [pkgs.any-nix-shell];};
+  programs.zsh = mkIf zsh-enable {
+    enable = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+  };
 }
-
