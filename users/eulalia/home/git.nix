aa5efa1644649dc563def1f14ca303265afe8b67
3ulalia <179992797+3ulalia@users.noreply.github.com>
2025-01-08 13:18:03 -0500

    remove useless git module

--- users/eulalia/home/git.nix
+++ users/eulalia/home/git.nix
@@ -1,14 +1,28 @@
 {
-  bootstrap,
   config,
-  lib,
-  pkgs,
   ...
-} : {
-
-  config.eula.modules.home-manager.git = {
-    user-name = "3ulalia";
-    user-email = "179992797+3ulalia@users.noreply.github.com";
+}: {
+  programs.git = {
+    # sane defaults
+    userName = "3ulalia";
+    userEmail = "179992797+3ulalia@users.noreply.github.com";
+    includes = map 
+      (x: 
+        {
+          path = config.sops.secrets."git-config/gh-${x}".path;
+          condition = "hasconfig:remote.*.url:git@gh-${x}*/**";
+        }
+      )
+      ["per" "pro" "sch"]
+    ;
+    extraConfig.init.defaultBranch = "main";
   };
+  
+  programs.gh.settings.git_protocol = "ssh";
 
+  sops.secrets = {
+    "git-config/gh-per" = {};
+    "git-config/gh-pro" = {};
+    "git-config/gh-sch" = {};
+  };
 }
