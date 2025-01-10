94467ade8274f3f0adfe6fc28ba48e6c7e6687e3
3ulalia <179992797+3ulalia@users.noreply.github.com>
2025-01-10 15:28:21 -0500

    move git ssh key config around

--- users/eulalia/home/git.nix
+++ users/eulalia/home/git.nix
@@ -17,7 +17,34 @@
     ;
     extraConfig.init.defaultBranch = "main";
   };
-  
+
+  programs.ssh.matchBlocks = {
+    github-personal = {
+      host = "gh-per";
+      hostname = "github.com";
+      identityFile = config.sops.secrets."ssh/gh-per".path;
+      identitiesOnly = true;
+    };
+    github-professional = {
+      host = "gh-pro";
+      hostname = "github.com";
+      identityFile = config.sops.secrets."ssh/gh-pro".path;
+      identitiesOnly = true;
+    };
+    /*github-school = {
+      host = "gh-sch";
+      hostname = "REDACTED"; #TODO: sops templates
+      identityFile = config.sops.secrets."ssh/gh-per".path;
+      identitiesOnly = true;
+    };*/
+  };
+
+  sops.secrets = {
+    "ssh/gh-per" = {};
+    "ssh/gh-sch" = {};
+    "ssh/gh-pro" = {};
+  };
+
   programs.gh.settings.git_protocol = "ssh";
 
   sops.secrets = {
--- users/eulalia/home/ssh.nix
+++ users/eulalia/home/ssh.nix
@@ -6,30 +6,5 @@
     enable = true;
     compression = true;
     includes = [ "config.d/*" ];
-    matchBlocks = {
-      github-personal = {
-        host = "gh-per";
-        hostname = "github.com";
-        identityFile = config.sops.secrets."ssh/gh-per".path;
-        identitiesOnly = true;
-      };
-      github-professional = {
-        host = "gh-pro";
-        hostname = "github.com";
-        identityFile = config.sops.secrets."ssh/gh-pro".path;
-        identitiesOnly = true;
-      };
-      github-school = {
-        host = "gh-sch";
-        hostname = "github.khoury.northeastern.edu";
-        identityFile = config.sops.secrets."ssh/gh-per".path;
-        identitiesOnly = true;
-      };
-    };
-  };
-  sops.secrets = {
-    "ssh/gh-per" = {};
-    "ssh/gh-sch" = {};
-    "ssh/gh-pro" = {};
   };
 }
