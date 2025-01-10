7a5a3b89aa5adb850938f054caa5bce476103f2f
3ulalia <179992797+3ulalia@users.noreply.github.com>
2025-01-10 15:28:38 -0500

    add nix-colors

--- flake.nix
+++ flake.nix
@@ -54,6 +54,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    nix-colors = {
+      url = "github:misterio77/nix-colors";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     "3ulalia-nvim" = {
     	url = "git+file:///home/eulalia/repos/3ulalia.nvim/";
 	inputs.nixpkgs.follows = "nixpkgs";
