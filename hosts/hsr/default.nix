dc69edcce71fb6aa7c67b906c00985755be641f5
3ulalia <179992797+3ulalia@users.noreply.github.com>
2025-01-15 23:07:58 -0500

    add hsr as a host

---
+++ hosts/hsr/default.nix
@@ -0,0 +1,88 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+{
+  lib,
+  inputs,
+  pkgs,
+  ...
+}: let
+  inherit (lib) trace;
+in {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    inputs.nixos-hardware.nixosModules.apple-t2
+  ];
+
+  hardware.firmware = [
+    (pkgs.stdenvNoCC.mkDerivation (final: {
+      name = "brcm-firmware";
+      src = ./firmware/brcm;
+      installPhase = ''
+        mkdir -p $out/lib/firmware/brcm
+        cp ${final.src}/* "$out/lib/firmware/brcm"
+      '';
+    }))
+  ];
+
+  # Bootloader.
+
+  boot.loader.efi = {
+    canTouchEfiVariables = true;
+    efiSysMountPoint = "/efi";
+  };
+
+  boot.loader.systemd-boot.configurationLimit = 25;
+  boot.loader.systemd-boot.enable = true;
+
+  services.mbpfan.enable = true;
+  services.mbpfan.aggressive = true;
+
+  #eula.modules.services.miracast.enable = true;
+  eula.modules.services.ssh.enable = true;
+
+  eula.modules.nixos.users = {
+    eulalia = {
+      privileged = true;
+    };
+  };
+
+  eula.modules.services.disko = {
+    enable = true;
+    disko-config = ./disko.nix;
+    needed-for-boot = ["/persist" "/var/log"];
+  };
+
+  eula.modules.nixos.bluetooth.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  services.udisks2.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+}
