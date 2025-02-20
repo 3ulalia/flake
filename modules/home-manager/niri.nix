654267b28efdb23e3d2e3c7a7805d06364e3a403
3ulalia <179992797+3ulalia@users.noreply.github.com>
2025-02-20 13:07:25 -0500

    weird niri fuckery

--- modules/home-manager/niri.nix
+++ modules/home-manager/niri.nix
@@ -7,13 +7,7 @@
   ...
 }: let
   inherit (lib) filter head split types mkIf trace mkAliasDefinitions;
-  inherit (osConfig.eula.lib.helpers) list-to-attrs;
   mkOpt = osConfig.eula.lib.options.mkOpt;
-
-  configs-to-enabled-settings = cfgs:
-    list-to-attrs (map
-      (n: {${config.eula.modules.home-manager.niri.${n}.pkg.pname}.enable = true;})
-      (filter (n: config.eula.modules.home-manager.niri.${n}.enable) cfgs));
 in {
   options.eula.modules.home-manager.niri = {
     enable = mkOpt types.bool false;
@@ -21,13 +15,9 @@ in {
   };
 
   config = mkIf config.eula.modules.home-manager.niri.enable {
-    home = let
-      opts = config.eula.modules.home-manager.niri;
-    in {
+    nixpkgs.overlays = [inputs.niri.overlays.niri];
+    home = {
       # home-manager settings
-
-      # TODO break pkgs out into own modules
-
       sessionVariables = {
         NIXOS_OZONE_WL = "1";
         ELECTRON_OZONE_PLATFORM_HINT = "wayland";
--- modules/nixos/niri.nix
+++ modules/nixos/niri.nix
@@ -1,16 +1,18 @@
 {
   config,
   inputs,
+  lib,
   ...
 }: let
   inherit (config.eula.lib.modules) any-user;
 in {
+
   imports = [inputs.niri.nixosModules.niri];
 
+  programs.niri.enable = true;#(any-user (user: user.eula.modules.home-manager.niri.enable) config.home-manager.users);
+
   nixpkgs.overlays = [inputs.niri.overlays.niri];
-
-  programs.niri.enable = any-user (user: user.eula.modules.home-manager.niri.enable) config.home-manager.users;
-
+  
   home-manager.sharedModules = [
     ({
       osConfig,
