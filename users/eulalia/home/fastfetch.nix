05b4a0e0562875294565ccdcaf2361d3a8f40b80
3ulalia <179992797+3ulalia@users.noreply.github.com>
2025-03-10 00:21:24 -0400

    comment

--- users/eulalia/home/fastfetch.nix
+++ users/eulalia/home/fastfetch.nix
@@ -16,6 +16,7 @@
         {
           type = "command";
           key = "Uptime";
+          # NOTE: replacing '%02d' with '%2d' makes the rendering cleaner - 08 -> 8. do i care?
           text = "echo $(printf '%02d' $((($(($(date +%s) - 1707777777)) / 60) % 60)))$(printf '%02d' $((($(($(date +%s) - 1707777777)) / 60 / 60) % 24)))$(($(($(date +%s) - 1707777777)) / 60 / 60 / 24))";
           format = "{#cyan}{1~4} days, {#magenta}{1~2,4} hours, {#white}{1~0,2} mins"; 
         }
