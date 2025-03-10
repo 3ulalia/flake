8520c4c61ee448dd9b692a431569813ad0dd2174
3ulalia <179992797+3ulalia@users.noreply.github.com>
2025-03-10 00:22:38 -0400

    further hm stuff + default editor

--- users/eulalia/home/neovim.nix
+++ users/eulalia/home/neovim.nix
@@ -1,6 +1,7 @@
 { inputs, pkgs, config, ... } : {
-    nixpkgs.overlays = [
-      inputs.eulalia-nvim.overlays.default
-    ];
+#    nixpkgs.overlays = [
+#      inputs.eulalia-nvim.overlays.default
+#    ];
+    home.sessionVariables.EDITOR = "nvim";
     home.packages = [ pkgs.nvim-pkg ];
   }
