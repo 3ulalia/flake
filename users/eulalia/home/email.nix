75eb36dab3fef5fa3617cfd40cab7a59d8bfc8af
3ulalia <179992797+3ulalia@users.noreply.github.com>
2025-03-10 00:24:18 -0400

    oh my god i can't believe i got outlook working with aerc

--- users/eulalia/home/email.nix
+++ users/eulalia/home/email.nix
@@ -6,15 +6,33 @@
 } : 
 let
 
+  oauth2-endpoints = {
+    "outlook.office365.com" = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
+  };
+
   secrets = config.eula.extras.read-sops ../../../secrets/eval-secrets.nix;
-  construct-email = {name, flavor ? "plain", primary ? false, ae-enable ? true} : rec {
+  construct-email = {name, flavor ? "plain", check-time ? "1m", primary ? false, oauth2 ? false, ae-enable ? true} : rec {
     realName = secrets.email.${name}.realname;
     address = secrets.email.${name}.address;
     userName = if (flavor != "plain") then address else builtins.elemAt (lib.splitString "@" address) 0;
     passwordCommand = "cat ${config.sops.secrets."email/${name}/password".path}";
     inherit primary flavor;
-    aerc = {
+    aerc = rec {
       enable = ae-enable;
+      extraConfig = {
+        check-mail = check-time;
+      };
+      imapAuth = if oauth2 && (flavor == "outlook.office365.com") then "xoauth2" else if oauth2 then "oauthbearer" else null;
+      smtpAuth = imapAuth;
+      imapOauth2Params = if oauth2 then {
+        client_id = "9e5f94bc-e8a4-4e73-b8be-63364c29d753"; # thunderbird client_id
+        #scope = null; # TODO ?
+        token_endpoint = oauth2-endpoints.${flavor};
+      } else null;
+      smtpOauth2Params = imapOauth2Params;
+      extraAccounts = if (flavor == "gmail.com") then {
+        use-gmail-ext = true;
+      } else {};
     };
     # should be overridden by anything trying to set the IMAP host; this is for "plain" accounts that use IMAP
     imap.host = lib.mkOverride 5000 "mail.${builtins.elemAt (lib.splitString "@" address) 1}";
@@ -23,13 +41,13 @@ let
     # same as above; anything trying to set the SMTP host will override this
     smtp.host = imap.host;
     # security!!
-    smtp.port = lib.mkDefault 587;
+    smtp.port = lib.mkDefault 465;
   };
 in {
   
   accounts.email.accounts = {
     personal = construct-email {name = "personal"; flavor = "gmail.com"; primary = true;};
-    school = construct-email {name = "school"; flavor = "outlook.office365.com";};
+    school = construct-email {name = "school"; flavor = "outlook.office365.com"; oauth2 = true;};
     professional = construct-email {name = "professional";};
   };
   
@@ -49,6 +67,9 @@ in {
     filters = {
       "text/plain" = "colorize";
       "text/html" = "html | colorize";
+    };
+    hooks = {
+      mail-received = "notify-send -t 5000 -a aerc \"[$AERC_ACCOUNT] from $AERC_FROM_NAME\" \"$AERC_SUBJECT\"";
     };
   };
   home.packages = [pkgs.dante pkgs.w3m]; 
