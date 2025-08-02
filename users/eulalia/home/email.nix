b4c947080ce310b6d39f1d0f8e9037164ee45f78
3ulalia <179992797+3ulalia@users.noreply.github.com>
2025-08-02 19:03:42 -0400

    fix outlook config!

--- users/eulalia/home/email.nix
+++ users/eulalia/home/email.nix
@@ -35,19 +35,13 @@
         then "oauthbearer"
         else null;
       smtpAuth = imapAuth;
-      imapOauth2Params =
-        if oauth2
-        then {
-          client_id = "9e5f94bc-e8a4-4e73-b8be-63364c29d753"; # thunderbird client_id
-          #scope = null; # TODO ?
-          token_endpoint = oauth2-endpoints.${flavor};
-        }
-        else null;
-      smtpOauth2Params = imapOauth2Params;
-      extraAccounts = {
+      extraAccounts = rec {
         use-gmail-ext = flavor == "gmail.com";
         check-mail = check-time;
         default = "INBOX";
+        source-cred-cmd = lib.mkIf oauth2 "oama access ${address}";
+        cache-headers = oauth2;
+        outgoing-cred-cmd = source-cred-cmd;
       };
       extraConfig.ui = {
         threading-enabled = thread;
@@ -58,13 +52,13 @@
       };
     };
     # should be overridden by anything trying to set the IMAP host; this is for "plain" accounts that use IMAP
-    imap.host = lib.mkOverride 5000 "mail.${builtins.elemAt (lib.splitString "@" address) 1}";
+    imap.host = lib.mkOverride 5000 "mail.${builtins.elemAt (lib.splitString "@" address) 1}${if oauth2 then "?" else ""}";
     # security!
     imap.port = lib.mkDefault 993;
     # same as above; anything trying to set the SMTP host will override this
     smtp.host = imap.host;
     # security!!
-    smtp.port = lib.mkDefault 465;
+    smtp.port = if oauth2 then 587 else lib.mkDefault 465;
   };
 in {
   accounts.email.accounts = {
@@ -104,5 +98,23 @@ in {
       mail-received = "notify-send -t 5000 -a aerc \"[$AERC_ACCOUNT] from $AERC_FROM_NAME\" \"$AERC_SUBJECT\"";
     };
   };
-  home.packages = [pkgs.dante pkgs.w3m];
+  home.packages = [pkgs.dante pkgs.w3m pkgs.oama pkgs.libsecret];
+
+  home.file."${config.xdg.configHome}/oama/config.yaml" = {
+    enable = true;
+    # onChange = "oama renew ${config.accounts.email.accounts.school.address}"; # TODO generalize
+    text = ''
+
+    encryption:
+      tag: KEYRING
+
+    services:
+      microsoft:
+        client_id: 9e5f94bc-e8a4-4e73-b8be-63364c29d753
+        auth_scope: https://outlook.office.com/IMAP.AccessAsUser.All
+          https://outlook.office.com/SMTP.Send
+          offline_access
+        tenant: common
+   '';
+  };
 }
