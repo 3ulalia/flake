a8a910b930ab202893a550b230f93cf14d155acf
3ulalia <179992797+3ulalia@users.noreply.github.com>
2025-08-02 19:04:26 -0400

    persist ssh keys

--- users/eulalia/home/ssh.nix
+++ users/eulalia/home/ssh.nix
@@ -3,5 +3,7 @@
     enable = true;
     compression = true;
     includes = ["config.d/*"];
+    extraConfig = "IdentityFile ${config.home.homeDirectory}/.ssh/id_ed25519_THIS_IS_INSECURE_AND_NEEDS_TO_BE_CHANGED";
   };
+  eula.modules.home-manager.impermanence.directories = [ ".ssh" ];
 }
