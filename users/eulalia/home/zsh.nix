ddffe7ce146fdc2e5d5fe22dd14d0b4b99d68c6c
3ulalia <179992797+3ulalia@users.noreply.github.com>
2026-01-14 12:04:30 -0500

    fix zsh backup clobbering

--- users/eulalia/home/zsh.nix
+++ users/eulalia/home/zsh.nix
@@ -3,13 +3,15 @@
     history = {
       extended = true;
       ignoreSpace = true;
+      # https://github.com/nix-community/impermanence/issues/233
+      path = "$HOME/.local/share/zsh/.zsh_history";
     };
     historySubstringSearch.enable = true;
     syntaxHighlighting.enable = true;
   };
 
-  eula.modules.home-manager.impermanence.files = [
-    ".zsh_history"
+  eula.modules.home-manager.impermanence.directories = [
+    ".local/share/zsh"
   ];
 
   /*
