5cc689515a023232f13e1c99cca66f7e7417634a
3ulalia <179992797+3ulalia@users.noreply.github.com>
2026-01-15 20:35:03 -0500

    modernize impermanence, for real

--- modules/home-manager/impermanence.nix
+++ modules/home-manager/impermanence.nix
@@ -1,7 +1,6 @@
 {
   config,
   eulib,
-  inputs,
   lib,
   ...
 }: let
@@ -10,18 +9,14 @@
 
   cfg = config.eula.modules.home-manager.impermanence;
 in {
-#  imports = [inputs.impermanence.homeManagerModules.impermanence]; # no longer needed!
-
   options.eula.modules.home-manager.impermanence = {
     enable = mkOpt types.bool false;
     directories = mkOpt (types.listOf types.str) [];
     files = mkOpt (types.listOf types.str) [];
-    allowOther = mkOpt types.bool true;
   };
 
   config = mkIf cfg.enable {
-  /* here goes nothing...
-    home.persistence."/persist${config.home.homeDirectory}" = {
+    home.persistence."/persist" = {
       enable = true;
       directories =
         [
@@ -36,8 +31,6 @@ in {
         [
         ]
         ++ cfg.files;
-#      allowOther = cfg.allowOther; # no longer needed!
     };
-*/
   };
 }
