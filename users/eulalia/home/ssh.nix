7f491460cb9b582d2048b6061aec310b8d3a072b
3ulalia <179992797+3ulalia@users.noreply.github.com>
2026-02-11 12:09:55 -0500

    add explorer

--- users/eulalia/home/ssh.nix
+++ users/eulalia/home/ssh.nix
@@ -1,9 +1,19 @@
-{config, ...}: {
+{ config, ... }:
+let
+  secrets = config.eula.extras.read-sops ../../../secrets/eval-secrets.nix;
+in
+{
   programs.ssh = {
     enable = true;
     matchBlocks."*".compression = true;
-    includes = ["config.d/*"];
+    includes = [ "config.d/*" ];
+    matchBlocks.explorer = {
+      inherit (secrets.ssh.explorer) hostname user;
+      identitiesOnly = true;
+      identityFile = config.sops.secrets."ssh/gh-per".path;
+    };
     # extraConfig = "IdentityFile ${config.home.homeDirectory}/.ssh/id_ed25519_THIS_IS_INSECURE_AND_NEEDS_TO_BE_CHANGED";
   };
+
   # eula.modules.home-manager.impermanence.directories = [ ".ssh" ]; # no longer needed!
 }
